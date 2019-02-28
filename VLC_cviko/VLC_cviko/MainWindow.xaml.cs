e99bbbfb4fa6dc23b6c741fea247dfac56cfd334
bounlfi15 <bounlfi15@sps-prosek.cz>
2019-02-28 11:25:17 +0100

    Added seek bar (not working)

--- VLC_cviko/VLC_cviko/MainWindow.xaml.cs
+++ VLC_cviko/VLC_cviko/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -21,48 +22,10 @@ namespace VLC_cviko
     /// </summary>
     public partial class MainWindow : Window
     {
-        private bool isPaused = false;
         public MainWindow()
         {
             InitializeComponent();
-
-            var vlcLibDirectory = new DirectoryInfo(System.IO.Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "libvlc", IntPtr.Size == 4 ? "win-x86" : "win-x64"));
-
-            var options = new string[]
-            {
-                // VLC options can be given here. Please refer to the VLC command line documentation.
-            };
-
-            MyControl.MediaPlayer.VlcLibDirectory = vlcLibDirectory;
-
-            MyControl.MediaPlayer.EndInit();
-
-            // Load libvlc libraries and initializes stuff. It is important that the options (if you want to pass any) and lib directory are given before calling this method.
-            MyControl.MediaPlayer.Play(new Uri("http://download.blender.org/peach/bigbuckbunny_movies/big_buck_bunny_480p_h264.mov"), options);
-        }
-
-        private void Pause_Click(object sender, RoutedEventArgs e)
-        {
-            if (isPaused)
-            {
-                PlayBtn.Content = "▶️";
-                MyControl.MediaPlayer.Play();
-            }
-            else
-            {
-                PlayBtn.Content = "⏸️";
-                MyControl.MediaPlayer.Pause();
-            }
-
-            isPaused = !isPaused;
-        }
-        private void Forward_Click(object sender, RoutedEventArgs e)
-        {
-            MyControl.MediaPlayer.Position += 10;
-        }
-        private void Backward_Click(object sender, RoutedEventArgs e)
-        {
-            
+            this.DataContext = new MainWindowViewModel(VideoView);
         }
     }
 }
---
+++ VLC_cviko/VLC_cviko/MainWindowViewModel.cs
@@ -0,0 +1,141 @@
+﻿using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vlc.DotNet.Core;
+using Vlc.DotNet.Wpf;
+
+namespace VLC_cviko
+{
+    class MainWindowViewModel : ViewModelBase
+    {
+        private VlcControl videoView;
+        private bool isPaused = false;
+
+        private double videoTime;
+
+        public double VideoTime
+        {
+            get
+            {
+                return videoTime;
+            }
+            set
+            {
+                videoTime = value;
+                RaisePropertyChanged("VideoTime");
+                KingCrimson(videoTime);
+            }
+        }
+
+
+        private RelayCommand playbackCommand;
+
+        public RelayCommand PlaybackCommand
+        {
+            get
+            {
+                return playbackCommand;
+            }
+            set
+            {
+                playbackCommand = value;
+                RaisePropertyChanged("PlaybackCommand");
+            }
+        }
+
+        private RelayCommand forwardCommand;
+
+        public RelayCommand ForwardCommand
+        {
+            get
+            {
+                return forwardCommand;
+            }
+            set
+            {
+                forwardCommand = value;
+                RaisePropertyChanged("ForwardCommand");
+            }
+        }
+
+        private RelayCommand backwardCommand;
+
+        public RelayCommand BackwardCommand
+        {
+            get
+            {
+                return backwardCommand;
+            }
+            set
+            {
+                backwardCommand = value;
+                RaisePropertyChanged("BackwardCommand");
+            }
+        }
+
+        public MainWindowViewModel(VlcControl videoView)
+        {
+            this.videoView = videoView;
+
+            initializeVlcControl();
+
+            PlaybackCommand = new RelayCommand(TheWorld, true);
+            ForwardCommand = new RelayCommand(MadeInHeaven, true);
+            BackwardCommand = new RelayCommand(Mandom, true);
+            videoView.MediaPlayer.TimeChanged += GoldExperienceRequiem;
+
+
+            videoView.MediaPlayer.Play(new Uri("http://download.blender.org/peach/bigbuckbunny_movies/big_buck_bunny_480p_h264.mov"));
+        }
+
+        private void GoldExperienceRequiem(object sender, VlcMediaPlayerTimeChangedEventArgs e)
+        {
+            VideoTime = videoView.MediaPlayer.Time;
+            Debug.WriteLine(VideoTime);
+        }
+
+        private void TheWorld()
+        {
+            if (isPaused)
+            {
+                videoView.MediaPlayer.Play();
+            }
+            else
+            {
+                videoView.MediaPlayer.Pause();
+            }
+
+            isPaused = !isPaused;
+        }
+
+        private void MadeInHeaven()
+        {
+            //videoView.MediaPlayer.Time
+        }
+
+        private void Mandom()
+        {
+
+        }
+
+        private void KingCrimson(double value)
+        {
+
+        }
+
+        private void initializeVlcControl()
+        {
+            var vlcLibDirectory = new DirectoryInfo(System.IO.Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "libvlc", IntPtr.Size == 4 ? "win-x86" : "win-x64"));
+
+            videoView.MediaPlayer.VlcLibDirectory = vlcLibDirectory;
+
+            videoView.MediaPlayer.EndInit();
+        }
+    }
+}
